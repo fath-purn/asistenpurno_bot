5f119a4d089ff7c2f7805a6fdbf8fe4acc734f67
fathpurn <talitakamilia345@gmail.com>
2023-03-09 19:00:27 +0700

    .

--- app.js
+++ app.js
@@ -207,9 +207,9 @@ bot.command('hapus', async (ctx) => {
 // Start the server
 if (process.env.NODE_ENV === "production") {
     // Use Webhooks for the production server
-    const app = express();
-    app.use(express.json());
-    app.use(webhookCallback(bot, "express"));
+    // const app = express();
+    // app.use(express.json());
+    // app.use(webhookCallback(bot, "express"));
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, async () => {
